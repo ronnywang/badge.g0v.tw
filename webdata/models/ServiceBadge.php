1418893517b82c9a75f2d6d672f3181b0b2b2fe5
Ronny Wang <ronnywang@gmail.com>
2022-09-13 22:30:45 +0800

    ServiceBadge->badge_hash 改成 bigint

--- webdata/models/ServiceBadge.php
+++ webdata/models/ServiceBadge.php
@@ -11,7 +11,7 @@ class ServiceBadge extends Pix_Table
         $this->_columns['service_id'] = ['type' => 'int'];
         $this->_columns['service_user'] = ['type' => 'int'];
         $this->_columns['badge_time'] = ['type' => 'int'];
-        $this->_columns['badge_hash'] = ['type' => 'int'];
+        $this->_columns['badge_hash'] = ['type' => 'bigint'];
         $this->_columns['brief'] = ['type' => 'text'];
         $this->_columns['data'] = ['type' => 'jsonb'];
 
