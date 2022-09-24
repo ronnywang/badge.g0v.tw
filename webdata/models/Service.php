c8307e4463e3d039a0934f7b5edebef1df306c97
Ronny Wang <ronnywang@gmail.com>
2022-09-24 13:57:39 +0800

    增加 Service->getData()

--- webdata/models/Service.php
+++ webdata/models/Service.php
@@ -1,11 +1,20 @@
 <?php
 
+class ServiceRow extends Pix_Table_Row
+{
+    public function getData()
+    {
+        return json_decode($this->data);
+    }
+}
+
 class Service extends Pix_Table
 {
     public function init()
     {
         $this->_name = 'service';
         $this->_primary = 'id';
+        $this->_rowClass = 'ServiceRow';
 
         $this->_columns['id'] = ['type' => 'int', 'auto_increment' => true];
         $this->_columns['service_id'] = ['type' => 'varchar', 'size' => 32];
