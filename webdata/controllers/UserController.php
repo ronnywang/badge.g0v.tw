9369eabcaea433dc1d7c1bd2675723c0fbcaf277
Ronny Wang <ronnywang@gmail.com>
2022-09-24 16:31:18 +0800

    增加領取成就和修改資料功能

--- webdata/controllers/UserController.php
+++ webdata/controllers/UserController.php
@@ -2,6 +2,100 @@
 
 class UserController extends Pix_Controller
 {
+    public function init()
+    {
+        if (!$sToken = Pix_Session::get('sToken')) {
+            $sToken = crc32(uniqid());
+            Pix_Session::set('sToken', $sToken);
+        }
+        $this->view->sToken = $sToken;
+    }
+
+    public function indexAction()
+    {
+        if (!$login_id = Pix_Session::get('login_id')) {
+            return $this->redirect('/');
+        }
+
+        if ($user = User::findByLoginID($login_id)) {
+            return $this->redirect('/_/user/edit');
+        }
+
+        $ids = Pix_Session::get('ids');
+        if (!$users = ServiceUser::searchByIds($ids)) {
+            return $this->alert('您目前還未有任何成就可以領取，請期待下一版本改版', '/');
+        }
+
+        return $this->redirect('/_/user/new');
+    }
+
+    public function newAction()
+    {
+        if (!$login_id = Pix_Session::get('login_id')) {
+            return $this->redirect('/');
+        }
+
+        if ($user = User::findByLoginID($login_id)) {
+            return $this->redirect('/_/user/edit');
+        }
+
+        $ids = Pix_Session::get('ids');
+        if (!$users = ServiceUser::searchByIds($ids)) {
+            return $this->alert('您目前還未有任何成就可以領取，請期待下一版本改版', '/');
+        }
+
+        $this->view->login_id = $login_id;
+        $this->view->ids = $ids;
+        $this->view->prefixs = ServiceUser::getUserIdPrefixByIds($ids);
+
+        if ($_POST) {
+            if ($_POST['sToken'] != $this->view->sToken) {
+                return $this->alert('stoken error', '/');
+            }
+
+            if (strlen($_POST['id']) < 2) {
+                return $this->alert('id 太短', '/_/user/new');
+            }
+            if (strlen($_POST['id']) > 16) {
+                return $this->alert('id 太長', '/_/user/new');
+            }
+            $id = $_POST['id'];
+            if ($this->view->prefixs) {
+                $prefixs = array_filter($this->view->prefixs, function($s) use ($id){
+                    return strpos($id, $s) === 0;
+                });
+                if (count($prefixs) == 0) {
+                    return $this->alert("id 必須以 " . implode(' 或 ', $prefixs) . " 開頭", '/_/user/new/');
+                }
+            }
+
+            try {
+                User::insert([
+                    'name' => $_POST['id'],
+                    'ids' => json_encode($ids),
+                    'data' => '{}',
+                ]);
+            } catch (Pix_Table_DuplicateException $e) {
+                return $this->alert('代號已經被使用了，請再更換代號', '/_/user/new');
+            }
+
+            return $this->alert('建立成功', '/_/user/edit');
+        }
+    }
+
+    public function editAction()
+    {
+        if (!$login_id = Pix_Session::get('login_id')) {
+            return $this->redirect('/');
+        }
+
+        if (!$user = User::findByLoginID($login_id)) {
+            return $this->redirect('/_/user/new');
+        }
+
+        $this->view->user = $user;
+    }
+
     public function loginAction()
 	{
         $client_id = getenv('SLACK_CLIENT_ID');
@@ -15,6 +109,18 @@ class UserController extends Pix_Controller
             "" // team
         );
         return $this->redirect($url);
+    }
+
+    public function showAction($params)
+    {
+        $name = $params[0];
+        if (!$name) {
+            return $this->redirect('/');
+        }
+        if (!$user = User::find_by_name($name)) {
+            return $this->redirect('/');
+        }
+        $this->view->user = $user;
     }
 
 	public function slackdoneAction()
@@ -56,7 +162,7 @@ class UserController extends Pix_Controller
         $ids[] = 'slack://' . $obj->user->id;
         $ids[] = $obj->user->email;
         Pix_Session::set('ids', $ids);
-        return $this->redirect('/');
+        return $this->redirect('/_/user/');
     }
 
     public function logoutAction()
--- webdata/models/ServiceUser.php
+++ webdata/models/ServiceUser.php
@@ -22,8 +22,26 @@ class ServiceUser extends Pix_Table
         $this->_columns['data'] = ['type' => 'jsonb'];
 
         $this->_relations['service'] = ['rel' => 'has_one', 'type' => 'Service', 'foreign_key' => 'service_id'];
+        $this->_relations['badges'] = ['rel' => 'has_many', 'type' => 'ServiceBadge', 'foreign_key' => 'service_user'];
 
         $this->addIndex('service_user', ['service_id', 'user_id'], 'unique');
+    }
+
+    public static function getUserIdPrefixByIds($ids)
+    {
+        $users = self::searchByIds($ids);
+        if (!$users) {
+            return [];
+        }
+
+        $names = [];
+        foreach ($users as $u) {
+            $n = preg_replace('#\s#', '', strtolower($u->getData()->name));
+            if ($n) {
+                $names[] = $n;
+            }
+        }
+        return array_unique($names);
     }
 
     public static function searchByIds($ids)
@@ -35,22 +53,12 @@ class ServiceUser extends Pix_Table
             return sprintf("(data->'hash_ids' @> '\"%s\"')", md5($id . 'g0vg0v'));
         }, $ids);
 
-        $ret = new StdClass;
-        $ret->users = [];
-        $ret->badges = [];
+        $users = [];
 
         foreach (ServiceUser::search(implode(' OR ', $terms)) as $service_user) {
-            $ret->users[$service_user->id] = $service_user;
+            $users[$service_user->id] = $service_user;
         }
 
-        if (!count($ret->users)) {
-            return;
-        }
-
-        foreach (ServiceBadge::search(1)->searchIn('service_user', array_keys($ret->users)) as $badge) {
-            $ret->badges[] = $badge;
-        }
-        $ret->users = array_values($ret->users);
-        return $ret;
+        return array_values($users);
     }
 }
