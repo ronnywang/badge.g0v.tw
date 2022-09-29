6c966776a9ddb4ca851969b8bc83e1bb8ace121a
Ronny Wang <ronnywang@gmail.com>
2022-09-29 22:29:49 +0800

    add hackpad

--- webdata/scripts/add-service.php
+++ webdata/scripts/add-service.php
@@ -3,6 +3,15 @@
 include(__DIR__ . '/../init.inc.php');
 
 Service::insert([
+    'service_id' => 'hackpad',
+    'data' => json_encode([
+        'name' => 'Hackpad',
+        'url' => 'https://g0v.hackpad.tw',
+        'start_at' => '2013-03-21',
+        'end_at' => '2022-09-29',
+    ]),
+]);
+Service::insert([
     'service_id' => 'hackmd',
     'data' => json_encode([
         'name' => 'HackMD',
