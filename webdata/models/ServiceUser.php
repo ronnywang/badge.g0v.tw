85df8ca930d4228422ae612a2b383a13d9d0572a
Ronny Wang <ronnywang@gmail.com>
2022-10-01 09:29:19 +0800

    加上連至服務的超連結

--- webdata/models/ServiceUser.php
+++ webdata/models/ServiceUser.php
@@ -2,6 +2,30 @@
 
 class ServiceUserRow extends Pix_Table_Row
 {
+    public function getLink()
+    {
+        $data = $this->getData();
+        $service = $this->service->service_id;
+        if ($service == 'hackpad') {
+            return null;
+        }
+        if ($service == 'github') {
+            if (preg_match('#^[0-9]+$#', $this->user_id)) {
+                return null;
+            }
+            return 'https://github.com/' . $this->user_id;
+        }
+        if ($service == 'hackmd') {
+            $id = rtrim(base64_encode(hex2bin(str_replace('-', '', $this->user_id))), '=');
+            $id = str_replace('+', '-', $id);
+            $id = str_replace('/', '_', $id);
+            return 'https://g0v.hackmd.io/@' . $id;
+        }
+        if ($service == 'slack') {
+            return 'https://g0v-tw.slack.com/team/' . $this->user_id;
+        }
+    }
+
     public function getData()
     {
         return json_decode($this->data);
